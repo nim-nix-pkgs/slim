b631532cc3bf063ea4a312cf280e0e2ee94aea17
hugosenari <hugosenari@gmail.com>
2022-08-20 21:05:53 -0300

    feat(nimpkgs): update lock

--- devel/flake.nix
+++ devel/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''nim package manager'';
+  description = ''nim package manager support requires in nimble task'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-slim-v0_1_7.flake = false;
-  inputs.src-slim-v0_1_7.ref   = "refs/tags/v0.1.7";
-  inputs.src-slim-v0_1_7.owner = "bung87";
-  inputs.src-slim-v0_1_7.repo  = "slim";
-  inputs.src-slim-v0_1_7.type  = "github";
+  inputs.src-slim-devel.flake = false;
+  inputs.src-slim-devel.ref   = "refs/heads/devel";
+  inputs.src-slim-devel.owner = "bung87";
+  inputs.src-slim-devel.repo  = "slim";
+  inputs.src-slim-devel.type  = "github";
   
-  inputs."hmisc".owner = "nim-nix-pkgs";
-  inputs."hmisc".ref   = "master";
-  inputs."hmisc".repo  = "hmisc";
-  inputs."hmisc".dir   = "v0_11_14";
-  inputs."hmisc".type  = "github";
-  inputs."hmisc".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."hmisc".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs."fusion".owner = "nim-nix-pkgs";
+  inputs."fusion".ref   = "master";
+  inputs."fusion".repo  = "fusion";
+  inputs."fusion".dir   = "v1_1";
+  inputs."fusion".type  = "github";
+  inputs."fusion".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fusion".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   inputs."compiler".owner = "nim-nix-pkgs";
   inputs."compiler".ref   = "master";
@@ -32,13 +32,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-slim-v0_1_7"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-slim-devel"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-slim-v0_1_7";
+    src  = deps."src-slim-devel";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
